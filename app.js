959eb18c0d4bfaafe42b8827c27f98ca22e0abb3
Rivibibu Praba$hwara <92269011+WKprabashwara@users.noreply.github.com>
2022-07-25 11:00:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -102,7 +102,7 @@ var mySwiper = new Swiper(".swiper-container", {
 
 
 // Disable copy content, prevent blacked out text, prevent "I" key, "J" key, "S" key + macOS, "U" key, "F12" key
-juery(document).ready(function(){
+jQuery(document).ready(function(){
   jQuery(function() {
         jQuery(this).bind("contextmenu", function(event) {
             event.preventDefault();
